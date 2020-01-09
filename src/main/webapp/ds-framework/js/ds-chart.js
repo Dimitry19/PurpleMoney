31e6f6d9119b967a0142977dc53e74d0746f57d4
Dimitri Sime <liondx@LionDx.home>
2020-01-09 02:03:50 +0100

    Correction de la date dans le widget des prets

--- src/main/webapp/ds-framework/js/ds-chart.js
+++ src/main/webapp/ds-framework/js/ds-chart.js
@@ -254,10 +254,7 @@ DSCharManager = function () {
 		$('#personalFound').html("€ "+widgetPersonalFound);
 		$('#totalLoan').html("€ "+widgetLoanAmount);
 		$('#totalSanction').html("€ "+widgetTotalSanction);
-		//$('#totalLoanMsg').html("Date:"+"<span class='mr-2 text-danger'>"+widgetLoanDate+"</span>");
 		$('#totalLoanMsg').html(widgetLoanDate);
-		//$('#totalLoanMsg').css({"margin-left": "86px","margin-bottom":"-28px"});
-		//'<span class="mr-2 text-danger">+widgetLoanDate</span>';
 	}
 
 	return {
